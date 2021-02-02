391c4508ce7a240ce04457ac69782b7925edf1af
Jaeyoung Lee <ljae1129@gmail.com>
2021-02-02 16:33:56 +0900

    boj 1927 수정

--- boj/boj_1927.py
+++ boj/boj_1927.py
@@ -13,7 +13,7 @@ for _ in range(t):
         if len(heap) == 0:
             print(0)
         else:
-            print(heapq.heappop(heap))
+            print(heapq.heappop(heap)[1])
     else:
-        heapq.heappush(heap, n)
+        heapq.heappush(heap, (-n, n))
         
