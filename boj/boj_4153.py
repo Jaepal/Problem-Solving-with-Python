8eb1f31586d3b115096f9a53975bdfde101262da
Jaeyoung Lee <ljae1129@gmail.com>
2021-07-02 17:04:25 +0900

    문제풀이 추가

---
+++ boj/boj_4153.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    l = sorted(list(map(int, input().split())))
+    if l[0] == 0:
+        break
+    if l[0]**2 + l[1]**2 == l[2]**2:
+        print('right')
+    else:
+        print('wrong')
